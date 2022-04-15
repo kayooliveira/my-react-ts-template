e24562407240e221265d99639b560e04a98f17d9
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-04-14 22:18:05 -0300

    Fixing getUserToken function, renaming Logout to clearToken and removing some other functions

--- src/Services/Auth/index.ts
+++ src/Services/Auth/index.ts
@@ -23,27 +23,22 @@ export const setUserToken = (token: string) => {
 export const getUserToken = () => {
     try {
         const token = localStorage.getItem('user-token')
-        return token && JSON.parse(token)
+        if (token) {
+            return JSON.parse(token)
+        }
+        return null
     } catch (error) {
         Notify.error()
     }
 }
 
-// Function responsible to auth the users
-export const AuthUser = () => {
-    return false
-}
-
-// Function responsible to register the user
-export const RegisterUser = () => {
-    return false
-}
-
-// Function responsible to logout the user
-export const Logout = () => {
+// Function responsible to clear the user token
+export const clearToken = () => {
     try {
         const token = localStorage.getItem('user-token')
-        token && localStorage.removeItem('user-token')
+        if (token) {
+            localStorage.removeItem('user-token')
+        }
     } catch (error) {
         Notify.error()
     }
